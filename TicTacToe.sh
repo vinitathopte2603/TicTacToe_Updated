48ed6ebaa8986fc71fc7665f0389d13dda6bef7f
Vinita thopte <vinitathopte1@gmail.com>
2020-05-05 09:58:47 +0530

    [Vinita]Added: begin with a toss to

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -6,11 +6,15 @@ declare -a board
 #constants
 PLAYER=X;
 COMPUTER=O;
+HEAD=1;
+TAIL=0;
 
-function initializingBoard(){
+board=(0,0,0,0,0,0,0,0,0)
 
-	board=(0,0,0,0,0,0,0,0,0)
-
-}
-
-initializingBoard
+checkingToss=$((RANDOM%2))
+if [[ $checkingToss == $HEAD ]]
+then
+	echo "Player play first"
+else
+	echo "computer play first"
+fi
