39f4df113b9c36461d436e609ebc736088e52e3a
Vinita thopte <vinitathopte1@gmail.com>
2020-05-05 09:49:26 +0530

    [Vinita]Added: Function for initializing board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,1 +1,16 @@
-echo "Welcome to Tic tac toe ...."
+
+#!/bin/bash -x
+
+echo "Welcome to Tic tac toe Game"
+
+declare -a board
+NUMBER_OF_ROWS=3
+NUMBER_OF_COLUMNS=3
+
+function initializingBoard(){
+
+	board=(0,0,0,0,0,0,0,0,0)
+
+}
+
+initializingBoard
