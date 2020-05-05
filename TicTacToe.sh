7dcd86c3006d4945f40a5524cb0fa3b6dbd33a26
Vinita thopte <vinitathopte1@gmail.com>
2020-05-05 10:02:56 +0530

    [Vinita]Added: printing board and start to play game

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash -x
 
 echo "Welcome to Tic tac toe Game"
@@ -8,13 +9,47 @@ PLAYER=X;
 COMPUTER=O;
 HEAD=1;
 TAIL=0;
+BOARD_POSITION=10;
 
-board=(0,0,0,0,0,0,0,0,0)
+for (( i=1 ; i<=$BOARD_POSITION; i++ ))
+do
+		board[$i]=0 
+done
 
-checkingToss=$((RANDOM%2))
-if [[ $checkingToss == $HEAD ]]
-then
-	echo "Player play first"
-else
-	echo "computer play first"
-fi
+
+function  printBoard(){
+
+	echo " -------------"
+	echo " | "${board[1]}" | "${board[2]}" | "${board[3]}" | "
+	echo " -------------"
+	echo " | "${board[4]}" | "${board[5]}" | "${board[6]}" | "
+	echo " -------------"
+	echo " | "${board[7]}" | "${board[8]}" | "${board[9]}" | "
+	echo " -------------"
+}
+
+
+function userInput() {
+   read -p "Enter your choice :" POSITION
+	if [ $POSITION -gt 10 ]
+	then
+		echo "Invalid Input"
+	fi 
+	board[$POSITION]=$PLAYER 
+	printBoard
+}
+
+function ticTacToe(){
+
+	checkingToss=$((RANDOM%2))
+	if [[ $checkingToss == $HEAD ]]
+	then
+		echo "Player play first"
+		printBoard
+		userInput
+	else
+		echo "computer play first"
+		printBoard
+	fi
+}
+ticTacToe
